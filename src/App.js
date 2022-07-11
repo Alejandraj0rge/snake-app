a1a73862d8e3d7b93d1906cd71ef405e232bca21
Alejandraj0rge <alejandra.jorge@hytechgizmo.com>
2022-07-11 16:36:22 -0400

    set coordenates for snake

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useReducer} from 'react';
+
+const PROPERTIES = {
+	FORMATING: {snake: 'snake-dot', cell: 'dot', food: 'food-dot'},
+	COLUMNS: 10,
+	ROWS: 10,
+	FOOD_COORDENATES: {column: Math.floor(Math.random() * 10), row: Math.floor(Math.random() * 10)}
+} 
+
+function manageCell(e){
+	console.log(e.currentTarget.dataset.column);
+}
+
+function getKey(e){
+	switch (e.code) {
+		case 'ArrowRight':
+		/* 	e.currentTarget.dataset.column
+			e.currentTarget.dataset.row
+			 */
+			break;
+
+		case 'ArrowLeft':
+			
+			break;
+
+		case 'ArrowUp':
+			
+			break;
+
+		case 'ArrowDown':
+			
+			break;
+	}
+}
+
+function reducer({row, column, cell}, action){
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [{row, column, cell}, dispatch] = useReducer(reducer, 0);
+	let board = [];
+
+	for(let x = 0; x < PROPERTIES.ROWS; x++) {
+		for(let i = 0; i < PROPERTIES.COLUMNS; i++) {
+			board.push(<div className='dot' id={i + '_snake_dot'}  data-column={i} data-row={x} onClick={manageCell}></div>);
+		}
+	}
+
+	board.forEach(element => {
+		/* (element.props.dataset.column == PROPERTIES.FOOD_COORDENATES.column) && (element.props.dataset.row == PROPERTIES.FOOD_COORDENATES.row) 
+				? */ element.props.className = PROPERTIES.FORMATING.food /* : null */;
+	});
+		
+	return (
+		<div className="App">
+		<body>
+			<div className='square'>
+				{board}
+			</div>
+			<input onKeyDown={getKey}></input>
+		</body>
+		</div>
+	);
 }
 
 export default App;
